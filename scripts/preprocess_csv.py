4121e12f8e7fd99c24f988f5065c7a064f1c8a55
José Padilla <jpadilla@webapplicate.com>
2020-05-09 18:26:00 -0400

    Remove more columns

--- scripts/preprocess_csv.py
+++ scripts/preprocess_csv.py
@@ -1,24 +1,67 @@
 import pandas as pd
 
-data = pd.read_csv("./assets/original-2007-2014.csv", low_memory=False)
-data.drop(["informad", "dadress"], axis=1, inplace=True)
-data.to_csv("./assets/2007-2014.csv", index=False)
 
-data = pd.read_csv("./assets/original-2015-2020.csv", low_memory=False)
-data.drop(
-    [
-        "ResidencePlaceAddress1",
-        "ResidencePlaceAddress2",
-        "InformantAddress1",
-        "InformantAddress2",
-        "DeathPlaceAddress1",
-        "DeathPlaceAddress2",
-        "CertifierAddress1",
-        "CertifierAddress2",
-        "InjuryOccurrenceAddress1",
-        "InjuryOccurrenceAddress2",
-    ],
-    axis=1,
-    inplace=True,
-)
-data.to_csv("./assets/2015-2020.csv", index=False)
+def remove_sensitive_columns():
+    data = pd.read_csv("./assets/original-2007-2014.csv", low_memory=False)
+    data.drop(
+        [
+            "dadress",
+            "dname",
+            "fname",
+            "fsurnam",
+            "fsurname",
+            "informad",
+            "informname",
+            "mname",
+            "msurnam",
+            "msurname",
+            "sponame",
+        ],
+        axis=1,
+        inplace=True,
+    )
+    data.to_csv("./assets/2007-2014.csv", index=False)
+
+    data = pd.read_csv("./assets/original-2015-2020.csv", low_memory=False)
+    data.drop(
+        [
+            "AKALastName",
+            "AKAMiddleName",
+            "AKAName",
+            "AKASecondLastName",
+            "CertifierAddress1",
+            "CertifierAddress2",
+            "CertifierName",
+            "DeathPlaceAddress1",
+            "DeathPlaceAddress2",
+            "EmbalmerName",
+            "FatherLastName",
+            "FatherMiddleName",
+            "FatherName",
+            "FatherSecondLastName",
+            "FuneralFacilityDirectorName",
+            "InformantAddress1",
+            "InformantAddress2",
+            "InformantName",
+            "InjuryOccurrenceAddress1",
+            "InjuryOccurrenceAddress2",
+            "LastName",
+            "MiddleName",
+            "MotherLastName",
+            "MotherMiddleName",
+            "MotherName",
+            "MotherSecondLastName",
+            "Name",
+            "ResidencePlaceAddress1",
+            "ResidencePlaceAddress2",
+            "SecondLastName",
+            "SpouseName",
+        ],
+        axis=1,
+        inplace=True,
+    )
+    data.to_csv("./assets/2015-2020.csv", index=False)
+
+
+if __name__ == "__main__":
+    remove_sensitive_columns()
